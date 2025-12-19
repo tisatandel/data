d6c251e7460fef25a23d3692d6b74b765ee38dd7
Tisa <tisatandel@gmail.com>
2025-12-19 18:35:53 +0530

    input output

--- src/app/contact/contact-form/contact-form.ts
+++ src/app/contact/contact-form/contact-form.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css',
 })
 export class ContactForm {
+  @Input() users: any[] = [];
 
+  @Output() edit = new EventEmitter<{ user: any; index: number }>();
+  @Output() delete = new EventEmitter<number>();
+
+  editUser(user: any, index: number) {
+    this.edit.emit({ user, index });
+  }
+
+  deleteUser(index: number) {
+    this.delete.emit(index);
+  }
 }
--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactForm } from './contact-form/contact-form';
+
+
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
-  imports: [],
+  imports: [FormsModule,ContactForm],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
+  standalone: true,
 })
-export class Contact {
+export class Contact{
+  users: any[] = [];
+
+  name = '';
+  email = '';
+  address = '';
+
+  isEditMode = false;
+  editIndex: number | null = null;
+
+  addOrUpdate() {
+    if (!this.name || !this.email || !this.address) return;
+
+    if (this.isEditMode && this.editIndex !== null) {
+      this.users[this.editIndex] = {
+        name: this.name,
+        email: this.email,
+        address: this.address
+      };
+    } else {
+      this.users.push({
+        name: this.name,
+        email: this.email,
+        address: this.address
+      });
+    }
+
+    this.reset();
+  }
+
+  editUser(data: { user: any; index: number }) {
+    this.isEditMode = true;
+    this.editIndex = data.index;
+
+    this.name = data.user.name;
+    this.email = data.user.email;
+    this.address = data.user.address;
+  }
+deleteUser(index: number) {
+  this.users.splice(index, 1);
+}
+
+
+  reset() {
+    this.name = '';
+    this.email = '';
+    this.address = '';
+    this.isEditMode = false;
+    this.editIndex = null;
+  }
 }
