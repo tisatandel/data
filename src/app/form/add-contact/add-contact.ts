00c9677350b8cd76d95e9f1a8d4ed389bd60f83a
Tisa <tisatandel@gmail.com>
2025-12-20 20:01:58 +0530

    all done

--- src/app/form/add-contact/add-contact.ts
+++ src/app/form/add-contact/add-contact.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './add-contact.html',
-  styleUrl: './add-contact.css',
+  styleUrls: ['./add-contact.css']
 })
 export class AddContact {
+  @Input() contact: any = null;
+  @Output() contactAdded = new EventEmitter<any>();
 
+  name = '';
+  phone = '';
+  email = '';
+
+  ngOnChanges() {
+    if (this.contact) {
+      this.name = this.contact.name;
+      this.phone = this.contact.phone;
+      this.email = this.contact.email;
+    } else {
+      this.reset();
+    }
+  }
+
+  addContact() {
+    this.contactAdded.emit({
+      name: this.name,
+      phone: this.phone,
+      email: this.email
+    });
+    this.reset();
+  }
+
+  reset() {
+    this.name = '';
+    this.phone = '';
+    this.email = '';
+  }
 }
