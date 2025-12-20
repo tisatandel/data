04dacd2e06a628f875ad8b83a1f30c04b55c0372
Tisa <tisatandel@gmail.com>
2025-12-20 19:52:59 +0530

    deleted

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { Contact } from "./contact/contact";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Contact],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
