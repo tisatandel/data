3ef490217d2a4fcca29f7adc7e41e3914b3ecdea
Tisa <tisatandel@gmail.com>
2025-12-20 19:58:04 +0530

    Component created

---
+++ src/app/form/add-contact/add-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contact',
+  imports: [],
+  templateUrl: './add-contact.html',
+  styleUrl: './add-contact.css',
+})
+export class AddContact {
+
+}
